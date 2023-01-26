933f90537faacbc80575622fd255ef34efc37206
Jose <josemanuel21302@gmail.com>
2023-01-26 01:03:11 -0800

    added comments

--- index.js
+++ index.js
@@ -14,16 +14,18 @@
 
 
 
-
-function newImage(src, left, bottom,) {
+// this function will spawn entities like how you would spawn a greenCharacter
+function newImage(src, left, bottom,) { // takes 3 parameters : Image source, left, right
     let newImage = document.createElement('img')
-    newImage.src = src
-    newImage.style.position = 'fixed'
+    newImage.src = src //property wil give it a new source from the parameter
+    newImage.style.position = 'fixed' //makes sure that it stays in place no matter where your scroll in the web page
     newImage.style.left = left + "px"
     newImage.style.bottom = bottom + "px"
-    document.body.append(newImage)
+    document.body.append(newImage) // appends the newImage to existance
 }
 
+
+// the lines below will create new Entities like: greenCharacter, pinetree, tree, pillar, crate, and well from the function above
 newImage('assets/green-character.gif', 100, 100)
 newImage("assets/pine-tree.png", 450, 200)
 newImage("assets/tree.png", 200, 300)
@@ -33,20 +35,27 @@ newImage("assets/well.png", 500, 425)
 
 //============Items under here==================
 
-
+//makes a new function so that we can spawn Items and pick them up by double clicking
 function newItem(src, left, bottom) {
-    let sword = document.createElement('img')
-    sword.src = src
-    sword.style.position = 'fixed'
-    sword.style.left = left + 'px'
-    sword.style.bottom = bottom + 'px'
-    document.body.append(sword)
+    //the sword variable is just a place holder variable
+    let item = document.createElement('img')
+    item.src = src
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
 
-    sword.addEventListener('dblclick', function(){
-        sword.remove()
+
+    //adding a event listener so that it will makje the item disapear from the page using .remove()
+    item.addEventListener('dblclick', function(){
+        item.remove()
     })
 
 }
+
+
+//yeah nothing to say here
+//call the item function and will spawn these items
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
